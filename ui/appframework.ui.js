2e7a3281f293adb19190c162d2f109d988ccec83
Philzen <phil@patient-radio.de>
2014-04-20 23:14:11 +0200

    remove (already removed) commented code
    Already removed before in https://github.com/01org/appframework/commit/6569ff4a1bd99035680944d4cca1c14e6b5a1ffb

--- ui/appframework.ui.js
+++ ui/appframework.ui.js
@@ -29,21 +29,17 @@
 
         this.availableTransitions = {};
         this.availableTransitions["default"] = this.availableTransitions.none = this.noTransition;
-        //setup the menu and boot touchLayer
+        // setup the menu and boot touchLayer
 
-        //Hack  for AMD/requireJS support
-        //set autolaunch = false
-        if ((typeof define === "function" && define.amd)||(typeof module !== "undefined" && module.exports)) {
+        // Hack for AMD/requireJS support
+        // set autolaunch = false
+        if ((typeof define === "function" && define.amd) || (typeof module !== "undefined" && module.exports)) {
             that.autoLaunch=false;
         }
 
-        /*if ($("#afui").length === 1) {
-            setupCustomTheme();
-        }*/
-
-        var setupAFDom=function(){
-            //boot touchLayer
-            //create afui element if it still does not exist
+        var setupAFDom = function(){
+            // boot touchLayer
+            // create afui element if it still does not exist
             var afui = document.getElementById("afui");
             if (afui === null) {
                 afui = document.createElement("div");
